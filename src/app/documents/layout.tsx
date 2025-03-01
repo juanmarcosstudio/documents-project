076dd56cfa7611d22e97f0fd3979090b683d1ec0
Jhon Mark <jhonmarktamayo134@gmail.com>
2025-03-02 00:02:28 +0700

    Minor chages

--- src/app/documents/layout.tsx
+++ src/app/documents/layout.tsx
@@ -5,7 +5,7 @@ interface DocumentsLayoutProps {
 const DocumentsLayout = ({children}: DocumentsLayoutProps) => {
     return ( 
         <div className="flex flex-col gap-y-4">
-            <div className="w-full bg-red-500">Document navbar</div>
+            <div className="w-full bg-red-500">Document navbar styling</div>
             {children}
         </div>
      );
