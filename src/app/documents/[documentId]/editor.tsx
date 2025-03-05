d575c4fb8e3c11812e2f6a5b990b44f2e738b33d
Jhon Mark <jhonmarktamayo134@gmail.com>
2025-03-06 00:14:14 +0700

    Import TaskItem and TaskList from Tiptap

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,17 +1,31 @@
 "use client";
 
 import { useEditor, EditorContent } from '@tiptap/react'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
 import StarterKit from '@tiptap/starter-kit'
 
 export const Editor = () => {
     const editor = useEditor({
-    extensions: [StarterKit],
+        editorProps: {
+            attributes: {
+                styles: "padding-left: 56px; padding-right: 56px;",
+                class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[105px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
+            },
+        },
+    extensions: [
+        StarterKit,
+        TaskItem,
+        TaskList,
+    ],
     content: '<p>Hello World! 🌎️</p>',
   })
 
     return (
-        <div>
-            <EditorContent editor={editor} />
+        <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+            <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
+                <EditorContent editor={editor} />
+            </div>
         </div>
     );
 };
