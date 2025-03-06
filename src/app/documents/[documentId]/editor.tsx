4f19c5eadfd8420f91e6450ae7ebbf802cb4f15b
Jhon Mark <jhonmarktamayo134@gmail.com>
2025-03-07 00:35:03 +0700

    feat(tiptap): add table extensions (Table, TableCell, TableHeader, TableRow)

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { useEditor, EditorContent } from '@tiptap/react'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
 import TaskItem from '@tiptap/extension-task-item'
 import TaskList from '@tiptap/extension-task-list'
 import StarterKit from '@tiptap/starter-kit'
@@ -10,11 +14,15 @@ export const Editor = () => {
         editorProps: {
             attributes: {
                 styles: "padding-left: 56px; padding-right: 56px;",
-                class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[105px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
+                class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
             },
         },
     extensions: [
         StarterKit,
+        Table,
+        TableCell,
+        TableHeader,
+        TableRow,
         TaskItem.configure({
             nested: true,
         }),
