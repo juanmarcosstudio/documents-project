54825d2b9d3089de17ec10f04755ce48cc279832
Jhon Mark <jhonmarktamayo134@gmail.com>
2025-03-13 00:55:17 +0700

    Add file for toolbar

---
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -0,0 +1,7 @@
+export const Toolbar = () => {
+    return (
+        <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-auto">
+            Toolbar
+        </div>
+    );
+};
