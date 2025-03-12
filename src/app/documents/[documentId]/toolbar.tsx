2d6e747451e13d1dd0b19c7a9598d35693206912
Jhon Mark <jhonmarktamayo134@gmail.com>
2025-03-13 01:16:54 +0700

    feat: add Toolbar and ToolbarButton components with undo functionality

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,3 +1,30 @@
+import { LucideIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+interface ToolbarButtonProps {
+    onClick?: () => void;
+    isActive?: boolean;
+    icon: LucideIcon;
+};
+
+const ToolbarButton = ({
+    onClick,
+    isActive,
+    icon: Icon,
+}: ToolbarButtonProps) => {
+    return (
+        <button
+            onClick={onClick}
+            className={cn(
+                "text-sm h-7 min-w-7 flex items-center justify-center rounded-sm hover:bg-neutral-200/80",
+                isActive && "bg-neutral-200/80"
+            )}
+        >
+            <Icon className="size-4" />
+        </button>
+    )
+}
+
 export const Toolbar = () => {
     return (
         <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-auto">
