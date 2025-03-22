005d7824a635b8578eba5d5e8256e46a8eb6a79c
Jhon Mark <jhonmarktamayo134@gmail.com>
2025-03-22 14:13:44 +0700

    Undo toolbar

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,5 +1,8 @@
-import { LucideIcon } from "lucide-react";
+"use client";
+
+import { LucideIcon, Undo2Icon } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { useEditorStore } from "@/store/use-editor-store";
 
 interface ToolbarButtonProps {
     onClick?: () => void;
@@ -26,9 +29,28 @@ const ToolbarButton = ({
 }
 
 export const Toolbar = () => {
+    const { editor } = useEditorStore();
+
+    const sections: {
+        label: string;
+        icon: LucideIcon;
+        onClick: () => void;
+        isActive?: boolean;
+    }[][] = [
+        [
+            {
+                label: "Undo",
+                icon: Undo2Icon,
+                onClick: () => editor?.chain().focus().undo().run(),
+            },
+        ],
+    ];
+
     return (
         <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-auto">
-            Toolbar
+            {sections[0].map((item) => (
+                <ToolbarButton key={item.label} {...item} />
+            ))}
         </div>
     );
 };
